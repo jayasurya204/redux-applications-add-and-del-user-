e641df0dc2ab8162c5564a17346c46aeffe3570e
jayasurya204 <muthusurya999@gmail.com>
2026-02-18 12:00:17 +0530

    Update CustomerSlice.jsx

--- src/Slices/CustomerSlice.jsx
+++ src/Slices/CustomerSlice.jsx
@@ -4,24 +4,24 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = [];
 
 const customerSlice = createSlice({
-    name: 'customer',
-    initialState,
-    reducers:{
-      addcustomer(state , action ){
-        state.push(action.payload)
-      },
-      editcustomer(state ,action){
-       const editIndex = action.payload
-       return state.find((val , index) => index) 
-      },
+  name: 'customer',
+  initialState,
+  reducers: {
+    addcustomer(state, action) {
+      state.push(action.payload)
+    },
+    editcustomer(state, action) {
+      const editIndex = action.payload
+      return state.find((val, index) => index) 
+    },
 
-      deleteCustomer(state , action){
-        const deleteIndex = action.payload;
-       return state.filter((val , index) => index !== deleteIndex )
-      }
-
+    deleteCustomer(state, action) {
+      const deleteIndex = action.payload;
+      return state.filter((val, index) => index !== deleteIndex)
     }
 
+  }
+
 })
-export const {addcustomer , deleteCustomer ,editcustomer} = customerSlice.actions;
+export const { addcustomer, deleteCustomer, editcustomer } = customerSlice.actions;
 export default customerSlice.reducer
